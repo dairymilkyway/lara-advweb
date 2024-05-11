c4a2edbf6ba63676f5729f928d2ca5741c75356c
Gwyn <skythekid211@gmail.com>
2024-05-11 09:11:41 +0800

    initial commit

---
+++ database/migrations/2024_05_11_001817_create_item_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('item', function (Blueprint $table) {
+            $table->id('item_id');
+            $table->string('description');
+            $table->string('sell_price');
+            $table->string('cost_price');
+            $table->string('img_path');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('item');
+    }
+};
