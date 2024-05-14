e8b646da33318d323d4a73ff9c877b0b6aa861aa
Gwyn <skythekid211@gmail.com>
2024-05-14 13:42:03 +0800

    fixed item

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -29,6 +29,7 @@ class ItemController extends Controller
         // $items = DB::table('item')->join('stock', 'item.item_id', '=', 'stock.item_id')->get();
         // $items = Item::has('stock')->get();
         // dd($items);
+        $items = Item::all();
         return view('item.index', compact('items'));
     }
 
