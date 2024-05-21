0eafef50ba1cc2d8e13dd1c63c4556629d5f9ff3
Gwyn <skythekid211@gmail.com>
2024-05-21 14:14:48 +0800

    new

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,13 +6,22 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Order;
 use App\Models\User;
+use Spatie\Searchable\Searchable;
+use Spatie\Searchable\SearchResult;
 
-class Customer extends Model
+class Customer extends Model implements Searchable
 {
     use HasFactory;
     protected $table = 'customer';
     protected $primaryKey = 'customer_id';
     public $timestamps = false;
+    public $fillable = [
+        'lname',
+        'fname',
+        'addressline',
+        'phone',
+        'zipcode',
+    ];
 
     public function user()
     {
@@ -21,5 +30,16 @@ class Customer extends Model
 
     public function orders() {
         return $this->hasMany(Order::class, 'customer_id');
+    }
+
+    public function getSearchResult(): SearchResult
+    {
+       $url = route('customers.edit', $this->customer_id);
+    
+        return new \Spatie\Searchable\SearchResult(
+           $this,
+           $this->lname. " ". $this->fname,
+           $url
+        );
     }
 }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -6,13 +6,21 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Order;
+use Spatie\Searchable\Searchable;
+use Spatie\Searchable\SearchResult;
 
-class Item extends Model
+class Item extends Model implements Searchable
 {
     use HasFactory;
     use SoftDeletes;
     protected $table = 'item';
     protected $primaryKey = 'item_id';
+    public $fillable = [
+        'description',
+        'sell_price',
+        'cost_price',
+        'img_path',
+    ];
     public function orders()
     {
         return $this->belongsToMany(
@@ -26,5 +34,16 @@ class Item extends Model
     public function stock()
     {
         return $this->hasOne(Stock::class, 'item_id');
+    }
+
+    public function getSearchResult(): SearchResult
+    {
+       $url = route('items.show', $this->item_id);
+    
+        return new \Spatie\Searchable\SearchResult(
+           $this,
+           $this->description,
+           $url
+        );
     }
 }
